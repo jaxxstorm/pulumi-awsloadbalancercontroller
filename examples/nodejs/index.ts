cd2b53c4a76f28f436c474a02e9c246ee6767d86
Lee Briggs <lee@leebriggs.co.uk>
2021-05-23 19:44:42 -0700

    fix example

--- examples/nodejs/index.ts
+++ examples/nodejs/index.ts
@@ -1,13 +1,12 @@
 import * as lb from "@jaxxstorm/pulumi-awsloadbalancercontroller";
 import * as k8s from "@pulumi/kubernetes"
 
+
+
 const loadbalancer = new lb.Deployment("example", {
-    //oidcIssuer: "oidc.eks.us-west-2.amazonaws.com/id/D4064024788B184AFFA7747591BD643D",
-    //oidcProvider: "arn:aws:iam::616138583583:oidc-provider/oidc.eks.us-west-2.amazonaws.com/id/D4064024788B184AFFA7747591BD643D",
-    oidcProvider: "arn:aws:iam::616138583583:oidc-provider/oidc.eks.us-west-2.amazonaws.com/id/6F5EB6A0B6482BE2960BD584BA77B7FB",
-    oidcIssuer: "oidc.eks.us-west-2.amazonaws.com/id/6F5EB6A0B6482BE2960BD584BA77B7FB",
+    oidcIssuer: "oidc.eks.us-west-2.amazonaws.com/id/D4064024788B184AFFA7747591BD643D",
+    oidcProvider: "arn:aws:iam::616138583583:oidc-provider/oidc.eks.us-west-2.amazonaws.com/id/D4064024788B184AFFA7747591BD643D",
     namespace: "aws-loadbalancer-controller",
     installCRDs: true,
-    //clusterName: "example-cluster",
-    clusterName: "nginx-helm-helm-eksCluster-4bae6a4"
+    clusterName: "example-cluster",
 })
